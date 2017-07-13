daa4eaac3f7ae108165e82f2c003128d818cefa2
danangnurfauzi <danangnurfauzi@gmail.com>
2017-07-13 22:50:04 +0700

    cetak mobile v1.0

--- app/src/main/java/com/example/danangnurfauzi/eticket/Dashboard.java
+++ app/src/main/java/com/example/danangnurfauzi/eticket/Dashboard.java
@@ -48,6 +48,8 @@ import com.example.danangnurfauzi.eticket.util.P25Connector;
 import java.lang.reflect.Method;
 import java.util.ArrayList;
 import java.util.Set;
+import java.util.*;
+import java.math.BigInteger;
 
 /**
  * Created by danangnurfauzi on 7/11/17.
@@ -151,7 +153,6 @@ public class Dashboard extends AppCompatActivity {
             public void onTextChanged(CharSequence s, int start, int before,
                                       int count) {
                 price.setText(hitungHargaTiket());
-
             }
 
             public void afterTextChanged(Editable s) {
@@ -251,9 +252,10 @@ public class Dashboard extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 //printDemoContent();
-                printStruk();
+                //printStruk();
                 //printStruk2();
                 //print1DBarcode();
+                printTiket();
             }
         });
 
@@ -337,9 +339,378 @@ public class Dashboard extends AppCompatActivity {
 
         total = (hargaWisatawanMancanegara * orang) + (hargaWisatawanNusantara * orang) + hargaKendaraanYa + hargaKendaraanTidak;
 
-        Toast.makeText(Dashboard.this,String.valueOf(total),Toast.LENGTH_LONG).show();
+        //Toast.makeText(Dashboard.this,String.valueOf(total),Toast.LENGTH_LONG).show();
 
         return Integer.toString(total);
+    }
+
+    private void printTiket() {
+
+        //calculate ticket price
+        int wisnu = 20000;
+        int wisman = 50000;
+        int hargaWisatawanNusantara;
+        int hargaWisatawanMancanegara;
+        int hargaWisatawan;
+
+        int kendaraanYa = 150000;
+        int kendaraanTidak = 0;
+        int hargaKendaraanYa;
+        int hargaKendaraanTidak;
+        int hargaKendaraan;
+
+        int orang;
+
+        int total;
+
+        String wawan;
+        String wiwin;
+
+        EditText munyuk = (EditText) findViewById(R.id.jumlah_orang);
+
+        //wisatawan
+        RadioButton wis = (RadioButton) findViewById(R.id.Pengunjung1);
+        if(wis.isChecked())
+        {
+            hargaWisatawanNusantara = wisnu;
+            wawan = "DOM";
+        }
+        else
+        {
+            hargaWisatawanNusantara = 0;
+            wawan = " ";
+        }
+
+        RadioButton jem = (RadioButton) findViewById(R.id.Pengunjung2);
+        if(jem.isChecked())
+        {
+            hargaWisatawanMancanegara = wisman;
+            wiwin = "MANCA";
+        }
+        else
+        {
+            hargaWisatawanMancanegara = 0;
+            wiwin = " ";
+        }
+
+        //kendaraan
+        RadioButton ken = (RadioButton) findViewById(R.id.Kendaraan1);
+        if(ken.isChecked())
+        {
+            hargaKendaraanYa = kendaraanYa;
+        }
+        else
+        {
+            hargaKendaraanYa = 0;
+        }
+
+        RadioButton tut = (RadioButton) findViewById(R.id.Kendaraan2);
+        if(tut.isChecked())
+        {
+            hargaKendaraanTidak = kendaraanTidak;
+        }
+        else
+        {
+            hargaKendaraanTidak = 0;
+        }
+
+        if(munyuk.getText().toString() != "" && munyuk.getText().length() > 0) {
+            orang = Integer.parseInt(munyuk.getText().toString());
+        } else {
+            orang = 0;
+        }
+
+        hargaWisatawan = (hargaWisatawanMancanegara * orang) + (hargaWisatawanNusantara * orang);
+        hargaKendaraan = hargaKendaraanYa + hargaKendaraanTidak;
+
+        total = (hargaWisatawanMancanegara * orang) + (hargaWisatawanNusantara * orang) + hargaKendaraanYa + hargaKendaraanTidak;
+
+        String receiptHead = "\n************************"+"\n"
+                + "   TIKET MASUK WISATA  "+"\n"
+                + "       KAWAH PUTIH  "+"\n"
+                + "************************" + "\n";
+
+        StringBuilder contentSb	= new StringBuilder();
+
+        contentSb.append("-----------------------------" + "\n");
+        contentSb.append("ORG       " + wawan + " " + wiwin + " " + orang +"  " + hargaWisatawan + "\n");
+        contentSb.append("MOBIL                "+ hargaKendaraan + "\n");
+        contentSb.append("-----------------------------" + "\n");
+        contentSb.append("TOTAL                  " + total + "\n");
+        contentSb.append("-----------------------------" + "\n");
+
+        long milis		= System.currentTimeMillis();
+        String date		= DateUtil.timeMilisToString(milis, "dd-MM-yy / HH:mm");
+
+        StringBuilder contentSb2	= new StringBuilder();
+
+        contentSb2.append("-----------------------------" + "\n");
+        contentSb2.append("TANGGAL   : " + date + "\n");
+        contentSb2.append("OPERATOR  : GANI GAIRAH A." + "\n");
+        contentSb2.append("-----------------------------" + "\n");
+
+        String message	= "Perhutani menyatakan struk ini  sebagai bukti pembayaran sah" + "\n";
+        String message2	= "Hubungi Call Center: 1 500 235 \n        www.perhutani.co.id" + "\n";
+        String message3 = "\n\n\n";
+
+        byte[] logo = StringUtil.hexStringToBytes("1b58 311f 6300 0000 0000 0000 0000 0000" +
+                "0000 0000 0000 0000 0000 01fc 0000 0000" +
+                "0000 0000 0000 0000 0000 0000 0000 0000" +
+                "0000 0000 0000 0000 0001 fff8 0000 0000" +
+                "0000 0000 0000 0000 0000 0000 0000 0000" +
+                "0000 0000 0000 0000 01ff fc00 0000 0000" +
+                "0000 0000 0000 0000 0000 0000 0000 0000" +
+                "0000 0000 0000 0000 fffc 0000 0000 0000" +
+                "0000 0000 0000 0000 0000 0000 0000 0000" +
+                "0000 0000 0000 00ff fc00 0000 0000 0000" +
+                "0000 0000 0000 0000 0000 0000 0000 0000" +
+                "0000 0000 0000 fffc 0000 0000 0000 0000" +
+                "0000 0000 0000 0000 0000 0000 0000 0000" +
+                "0000 0000 01ff fc00 0000 0000 0000 0000" +
+                "0000 0000 0000 0000 0000 0000 0000 0000" +
+                "0000 0003 fffc 0000 0000 0000 0000 0000" +
+                "0000 0000 0000 0000 0000 0000 0000 0000" +
+                "0000 07ff fc00 0000 0000 0000 0000 0000" +
+                "0000 0000 0000 0000 0000 0000 0000 0000" +
+                "000f fffc 0000 0000 0000 0000 0000 0000" +
+                "0000 0000 0000 0000 0000 0000 0000 0000" +
+                "1fff fc00 0000 0000 0000 0000 0000 0000" +
+                "0000 0000 0000 0000 0000 0000 0000 003f" +
+                "fffc 0000 0000 0000 0000 0000 0000 0000" +
+                "0000 0000 0000 0000 0000 0000 0000 3fff" +
+                "fc00 0000 0000 0000 0000 0000 0000 0000" +
+                "0000 0000 0000 0000 0000 0000 007f fffc" +
+                "0000 0000 0000 0000 0000 0000 0000 0000" +
+                "0000 0000 0000 0000 0000 0000 ffff fc00" +
+                "0000 0000 0000 0000 0000 0000 0000 0000" +
+                "0000 0000 0000 0000 0000 01ff ffbc 0000" +
+                "0000 0000 0000 0000 0000 0000 0000 0000" +
+                "0000 0000 0000 0000 0001 fffe 1c00 0000" +
+                "0000 0000 0000 0000 0000 0000 0000 0000" +
+                "0000 0000 0000 0000 03ff fc0c 07fc 0000" +
+                "0000 0000 0000 0000 0000 0000 0000 0000" +
+                "0000 0000 0000 0007 fff8 007f ffc0 0000" +
+                "0000 0000 0000 0000 0000 0000 0000 0000" +
+                "0000 0000 0000 07ff f800 ffff f000 0000" +
+                "0000 0000 0000 0000 0000 0000 0000 0000" +
+                "0000 0000 000f fff0 03ff fffc 0000 0000" +
+                "0000 0000 0000 0000 0000 0000 0000 0000" +
+                "0000 0000 0fff e007 ffff fe00 0000 0000" +
+                "0000 0000 0000 0000 0000 0000 0000 0000" +
+                "0000 001f ffc0 0fff ffff 0000 0000 0000" +
+                "0000 0000 0000 0000 0000 0000 0000 0000" +
+                "0000 1fff 801f ffff ff80 0000 0000 0000" +
+                "0000 0000 0000 0000 0000 0000 0000 0000" +
+                "003f ff00 3fff ffff c000 0000 0000 0000" +
+                "0000 0000 0000 0000 0000 0000 0000 0000" +
+                "3fff 007f ffff ffe0 0000 0000 0000 0000" +
+                "0000 0000 0000 0000 0000 0000 0000 007f" +
+                "fe00 7fff ffff f000 0000 0000 0000 0000" +
+                "0000 0000 0000 0000 0000 0000 0000 7ffc" +
+                "007f ff07 fff8 0000 0000 0000 0000 0000" +
+                "0000 0000 0000 0000 0000 0000 007f fc00" +
+                "fffc 01ff fc00 0000 0000 0000 0000 0000" +
+                "0000 0000 0000 0000 0000 0000 fff8 00ff" +
+                "f800 7ffc 0000 0000 0000 0000 0000 0000" +
+                "0000 0000 0000 0000 0000 00ff f800 fff0" +
+                "003f fe00 0000 0000 0000 0000 0000 0000" +
+                "0000 0000 0000 0000 0000 fff0 0000 0000" +
+                "1ffe 0000 0000 0000 0000 0000 0000 0000" +
+                "0000 0000 0000 0000 01ff f000 0000 000f" +
+                "ff00 0000 0000 0000 0000 0000 0000 0000" +
+                "0000 0000 0000 0001 fff0 0000 0000 07ff" +
+                "0000 0000 0000 0000 0000 0000 0000 0000" +
+                "0000 0000 0000 01ff e000 0060 0007 ff00" +
+                "0000 0000 0000 0000 0000 0000 0000 0000" +
+                "0000 0000 0001 ffe0 0078 f000 03ff 8000" +
+                "0000 0000 0000 0000 0000 0000 0000 0000" +
+                "0000 0000 03ff e000 7cf8 0003 ff80 0000" +
+                "0000 0000 0000 0000 0000 0000 0000 0000" +
+                "0000 0003 ffe0 007c f800 03ff 8000 0000" +
+                "0000 0000 0000 0000 0000 0000 0000 0000" +
+                "0000 03ff e00e 78f1 c001 ffc0 0000 0000" +
+                "0000 0000 0000 0000 0000 0000 0000 0000" +
+                "0003 ffc0 0f00 03c0 01ff c000 0000 0000" +
+                "0000 0000 0000 0000 0000 0000 0000 0000" +
+                "03ff c01f 0383 e001 ffc0 0000 0000 0000" +
+                "0000 0000 0000 0000 0000 0000 0000 0003" +
+                "ffc0 1f07 83c0 01ff c000 0000 0000 0000" +
+                "0000 0000 0000 0000 0000 0000 0000 03ff" +
+                "c00e 07c1 8001 ffc0 0000 0000 0000 0000" +
+                "0000 0000 0000 0000 0000 0000 0003 ffc0" +
+                "0007 c000 01ff c000 0000 0000 0000 0000" +
+                "0000 0000 0000 0000 0000 0000 03ff c070" +
+                "c38c 3801 ffc0 0000 0000 0000 0000 0000" +
+                "0000 0000 0000 0000 0000 0003 ffc0 79e0" +
+                "1e7c 01ff c000 0000 0000 0000 0000 0000" +
+                "0000 0000 0000 0000 0000 03ff c0f9 f79e" +
+                "7c01 ffc0 0000 0000 0000 0000 0000 0000" +
+                "0000 0000 0000 0000 0003 ffc0 f9ff de78" +
+                "01ff c000 0000 0000 0000 0000 0000 0000" +
+                "0000 0000 0000 0000 03ff e071 efde 3801" +
+                "ffc0 0000 0000 0000 0000 0000 0000 0000" +
+                "0000 0000 0000 0003 ffe0 000f c000 01ff" +
+                "c000 0000 0000 0000 0000 0000 0000 0000" +
+                "0000 0000 0000 01ff e000 0780 0001 ffc0" +
+                "0000 0000 0000 0000 0000 0000 0000 0000" +
+                "0000 0000 0001 ffe0 1c70 70f0 01ff c000" +
+                "0000 0000 0000 0000 0000 0000 0000 0000" +
+                "0000 0000 01ff f03c f878 f801 ffc0 0000" +
+                "0000 0000 0000 0000 0000 0000 0000 0000" +
+                "0000 0001 fff0 3ef8 f8f8 03ff 8000 0000" +
+                "0000 0000 0000 0000 0000 0000 0000 0000" +
+                "0000 00ff f83c f8f8 f003 ff80 0000 0000" +
+                "0000 0000 0000 0000 0000 0000 0000 0000" +
+                "0000 fff8 1870 7060 07ff 8000 0000 0000" +
+                "0000 0000 0000 0000 0000 0000 0000 0000" +
+                "00ff fc01 8000 0007 ff00 0000 0000 0000" +
+                "0000 0000 0000 0000 0000 0000 0000 0000" +
+                "7ffc 03c0 1e00 0fff 0000 0000 0000 0000" +
+                "0000 0000 0000 0000 0000 0000 0000 007f" +
+                "fe03 e31f 000f ff00 0000 0000 0000 0000" +
+                "0000 0000 0000 0000 0000 0000 0000 3fff" +
+                "03c7 9f00 1ffe 0000 0000 0000 0000 0000" +
+                "0000 0000 0000 0000 0000 0000 003f ff03" +
+                "cf9e 003f fe00 0000 0000 0000 0000 0000" +
+                "0000 0000 0000 0000 0000 0000 1fff 8007" +
+                "8000 7ffc 0000 0000 0000 0000 0000 0000" +
+                "0000 0000 0000 0000 0000 001f ffc0 0780" +
+                "007f fc00 0000 0000 0000 0000 0000 0000" +
+                "0000 0000 0000 0000 0000 0fff f000 0001" +
+                "fff8 0000 0000 0000 0000 0000 0000 0000" +
+                "0000 0000 0000 0000 000f fff8 0000 03ff" +
+                "f000 0000 0000 0000 0000 0000 0000 0000" +
+                "0000 0000 0000 0000 07ff fe00 000f ffe0" +
+                "0000 0000 0000 0000 0000 0000 0000 0000" +
+                "0000 0000 0000 0003 ffff 8000 3fff e000" +
+                "0000 0000 0000 0000 0000 0000 0000 0000" +
+                "0000 0000 0000 01ff fff0 01ff ffc0 0000" +
+                "0000 0000 0000 0000 0000 0000 0000 0000" +
+                "0000 0000 0000 ffff ffff ffff 8000 0000" +
+                "0000 0000 0000 0000 0000 0000 0000 0000" +
+                "0000 0000 007f ffff ffff fe00 0000 0000" +
+                "0000 0000 0000 0000 0000 0000 0000 0000" +
+                "0000 0000 3fff ffff fffc 0000 0000 0000" +
+                "0000 0000 0000 0000 0000 0000 0000 0000" +
+                "0000 001f ffff ffff f800 0000 0000 0000" +
+                "0000 0000 0000 0000 0000 0000 0000 0000" +
+                "0000 07ff ffff fff0 0000 0000 0000 0000" +
+                "0000 0000 0000 0000 0000 0000 0000 0000" +
+                "0001 ffff ffff c000 0000 0000 0000 0000" +
+                "0000 0000 0000 0000 0000 0000 0000 0000" +
+                "00ff ffff ff00 0000 0000 0000 0000 0000" +
+                "0000 0000 0000 0000 0000 0000 0000 0000" +
+                "1fff fffc 0000 0000 0000 0000 0000 0000" +
+                "0000 0000 0000 0000 0000 0000 0000 0003" +
+                "ffff e000 0000 0000 0000 0000 0000 0000" +
+                "0000 0000 0000 0000 0000 0000 0000 000f" +
+                "fc00 0000 0000 0000 0000 0000 0000 0000" +
+                "0000 0000 0000 0000 0000 0000 0000 0000" +
+                "0000 0000 0000 0000 0000 0000 0000 0000" +
+                "0000 0000 0000 0000 0000 0000 0000 0000" +
+                "0000 0000 0000 0000 0000 0000 0000 0000" +
+                "0000 0000 0000 0000 0000 0000 0000 0000" +
+                "0000 0000 0000 0000 0000 0000 0000 0000" +
+                "0000 0000 0000 0000 0000 0000 0000 0000" +
+                "0000 0000 0000 0000 0000 0000 0000 0000" +
+                "0000 0000 0000 0000 0000 0000 0000 0000" +
+                "0000 0000 0000 0000 0000 0000 0000 0000" +
+                "0000 0000 0000 0000 0000 0000 0000 0000" +
+                "0000 0000 0000 0000 0000 0000 0000 0000" +
+                "0000 0000 0000 0000 0000 0000 0000 0000" +
+                "0000 0000 0000 0000 0000 0000 0000 0000" +
+                "0001 fc3f f3fc 181d c0cf fe10 101c c000" +
+                "0000 0000 0000 0000 0000 0000 0000 0000" +
+                "03ff 3ff3 fe18 1dc0 cffe 3838 1cc0 0000" +
+                "0000 0000 0000 0000 0000 0000 0000 0003" +
+                "ff3f f3ff 181d c0cf fe78 3c1c c000 0000" +
+                "0000 0000 0000 0000 0000 0000 0000 0303" +
+                "b003 0398 1dc0 c0e0 7c3e 1cc0 0000 0000" +
+                "0000 0000 0000 0000 0000 0000 0003 03b0" +
+                "0303 981d c0c0 e07c 3e1c c000 0000 0000" +
+                "0000 0000 0000 0000 0000 0000 0303 b003" +
+                "0398 1dc0 c0e0 ec3f 1cc0 0000 0000 0000" +
+                "0000 0000 0000 0000 0000 0003 ff3f e3ff" +
+                "1ffd c0c0 e0ce 3b9c c000 0000 0000 0000" +
+                "0000 0000 0000 0000 0000 03ff 3fe3 fe1f" +
+                "fdc0 c0e1 c639 dcc0 0000 0000 0000 0000" +
+                "0000 0000 0000 0000 0003 fc30 03fe 181d" +
+                "c0c0 e1ff 38dc c000 0000 0000 0000 0000" +
+                "0000 0000 0000 0000 0300 3003 0e18 1dc0" +
+                "c0e1 ff38 fcc0 0000 0000 0000 0000 0000" +
+                "0000 0000 0000 0003 0030 0307 181c c1c0" +
+                "e383 387c c000 0000 0000 0000 0000 0000" +
+                "0000 0000 0000 0300 3ff3 0718 1ce3 80e3" +
+                "03b8 3cc0 0000 0000 0000 0000 0000 0000" +
+                "0000 0000 0003 003f f303 981c 7f80 e701" +
+                "b81c c000 0000 0000 0000 0000 0000 0000" +
+                "0000 0000 0300 3ff3 0198 1c3e 00e6 01f8" +
+                "18c0 ");
+
+        byte[] titleByte	= Printer.printfont(receiptHead, FontDefine.FONT_32PX,FontDefine.Align_CENTER,
+                (byte)0x1A, PocketPos.LANGUAGE_ENGLISH);
+
+        byte[] content1Byte	= Printer.printfont(contentSb.toString(), FontDefine.FONT_24PX,FontDefine.Align_LEFT,
+                (byte)0x1A, PocketPos.LANGUAGE_ENGLISH);
+
+        byte[] content2Byte	= Printer.printfont(contentSb2.toString(), FontDefine.FONT_24PX,FontDefine.Align_LEFT,
+                (byte)0x1A, PocketPos.LANGUAGE_ENGLISH);
+
+        byte[] messageByte	= Printer.printfont(message, FontDefine.FONT_24PX,FontDefine.Align_CENTER,  (byte)0x1A,
+                PocketPos.LANGUAGE_ENGLISH);
+
+        byte[] message2Byte	= Printer.printfont(message2, FontDefine.FONT_24PX,FontDefine.Align_CENTER,  (byte)0x1A,
+                PocketPos.LANGUAGE_ENGLISH);
+
+        byte[] message3Byte	= Printer.printfont(message3, FontDefine.FONT_24PX,FontDefine.Align_CENTER,  (byte)0x1A,
+                PocketPos.LANGUAGE_ENGLISH);
+
+        String contong = "1307170000014";
+
+        String fak = toHex(contong);
+
+        //byte[] barcode = StringUtil.hexStringToBytes("1d 6b 02 0d 36 39 30 31 32 33 34 35 36 37 38 39 32");
+
+        byte[] barcode = StringUtil.hexStringToBytes("1d 6b 02 0d"+fak);
+
+        byte[] totalByte	= new byte[logo.length + titleByte.length + content1Byte.length + messageByte.length +
+                message2Byte.length + content2Byte.length + barcode.length + message3Byte.length];
+
+
+        int offset = 0;
+        System.arraycopy(logo, 0, totalByte, offset, logo.length);
+        offset+=logo.length;
+
+        System.arraycopy(titleByte, 0, totalByte, offset, titleByte.length);
+        offset += titleByte.length;
+
+        System.arraycopy(content1Byte, 0, totalByte, offset, content1Byte.length);
+        offset += content1Byte.length;
+
+        System.arraycopy(content2Byte, 0, totalByte, offset, content2Byte.length);
+        offset += content2Byte.length;
+
+        System.arraycopy(messageByte, 0, totalByte, offset, messageByte.length);
+        offset += messageByte.length;
+
+        System.arraycopy(message2Byte, 0, totalByte, offset, message2Byte.length);
+        offset += message2Byte.length;
+
+        System.arraycopy(barcode, 0, totalByte, offset, barcode.length);
+        offset+=barcode.length;
+
+        System.arraycopy(message3Byte, 0, totalByte, offset, message3Byte.length);
+        offset += message3Byte.length;
+
+        //System.arraycopy(dateByte, 0, totalByte, offset, dateByte.length);
+
+        byte[] senddata = PocketPos.FramePack(PocketPos.FRAME_TOF_PRINT, totalByte, 0, totalByte.length);
+
+        sendData(senddata);
+    }
+
+    public String toHex(String arg) {
+        return String.format("%26x", new BigInteger(1, arg.getBytes(/*YOUR_CHARSET?*/)));
     }
 
     public boolean onCreateOptionsMenu(Menu menu){
